86637bfea5ab33ccca0e9c1c34c580b0f081862e
sunyanyanlearnmse <71137806+sunyanyanlearnmse@users.noreply.github.com>
2022-04-27 18:05:12 +0800

    change

--- js/pet.js
+++ js/pet.js
@@ -1,19 +1,22 @@
 var banner=document.getElementById('banner');
 var prev=document.getElementById('prev');
 var next=document.getElementById('next');
-var lis=document.querySelectorAll('+content>li');
-var dots=document.querySelectorAll('navv>li');
+var lis=document.querySelectorAll('#content>li');
+var dots=document.querySelectorAll('#navv>li');
 var index=0;
+//点击next
 next.onclick=nextFun;
-function nextFun (){
-  lis[index].style.opacity=0;
-  index++;
-  if(index==3){
-    index=0;   
-  }
-  lis[index].style.opacity=1;
-  changedot();
+function nextFun(){
+    lis[index].style.opacity =0;
+    index++;
+    if(index==3){
+      index=0;   
+    }
+    lis[index].style.opacity =1;
+    changeDot();
 }
+    
+  //点击 prev
 prev.onclick=function(){
   lis[index].style.opacity=0;
   index--;
@@ -21,30 +24,31 @@ prev.onclick=function(){
     index=2;    
   }
   lis[index].style.opacity=1;
-  changedot();
+  changeDot();
 }
-
-
-function changedot(){
+//按钮颜色变化
+function changeDot(){
      for (var i=0;i<dots.length;i++){
           dots[i].style.backgroundcolor ='#fff';
      }
-     dots[index].style.backgroundcolor='#f00'
+     dots[index].style.backgroundcolor='#f00';
     }
-
+//定时器 过2秒自动切换图片
 var timer = setInterval(nextFun,2000);
-
+//鼠标移入
 banner.onmouseover=function(){
   prev.style.opacity=1;
   next.style.opacity=1;
   clearInterval(timer);
 }
+//鼠标移出
 banner.onmouseout=function(){
   prev.style.opacity=0;
   next.style.opacity=0;
   timer = setInterval(nextFun,2000);
   
 }
+//为li添加鼠标移入事件
 for (var i=0;i<dots.length;i++){
      dots[i].pos=i;
 }
@@ -53,7 +57,7 @@ for (var i=0; i<dots.length;i++){
       lis[index].style.opacity=0;
       index=this.pos;
       lis[index].style.opacity=1;
-      changedot();
+      changeDot();
        
     }
 }
